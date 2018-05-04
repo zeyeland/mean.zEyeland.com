53199eeda593b470a73098146d1154121bb69d8d
Zyris Shakir <zeyeland@gmail.com>
2018-05-04 11:20:41 -0500

    Install mongoose

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//call the config file from config.dev.js
+var config = require('./config.dev')
+console.log(config);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
